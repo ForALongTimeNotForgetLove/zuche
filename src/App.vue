0360b4609be7bda02d27f90d5dae362e1bd0ff8e
这是啥’ <>
2020-11-15 21:34:39 +0800

    xxx

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="css">
+
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      home
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="css">
+
+</style>
+
